19909d2e2a62cbd937e7ffeb38e78d3f0d04ef49
GJ241211 <55745197+GJ241211@users.noreply.github.com>
2020-03-26 12:09:20 +0000

    26/03/2020
    Meeting on movement.

---
+++ Assets/YourProjectName/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+    [SerializeField]
+    float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        //rigidbody.velocity = new Vector2(-horizontalAxis * speed, 0);
+        rigidbody.AddForce(new Vector2(-horizontalAxis * speed, 0),ForceMode2D.Force);
+
+    }
+}
